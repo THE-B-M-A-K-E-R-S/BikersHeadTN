dd555963a4bc88db7641dd8aaebefd1baea03db0
BASSEM <bassem.jadoui@esprit.tn>
2022-10-04 23:38:35 +0100

    add dashboard blade ✅✅

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::get('/page2/{name?}/{age?}', function ($name = null, $age = null) {
 
 url('page2' , ['name' => 'foulen' , 'age'=> null ]);
 
+Route::get('/dashboard', function () {
+    return  view('dashboard');
+})->middleware('auth');
+
 
 
 
